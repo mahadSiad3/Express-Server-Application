5a6a55252d4852169cb01d8e7a960ba4f6cc60b2
mahadsiad3 <mahadsiad3@gmail.com>
2025-06-15 22:24:32 -0400

    adding additional 500 error

--- index.js
+++ index.js
@@ -7,6 +7,19 @@ import commentRoutes from './comments.js';
 const app = express();
 const port = 3000;
 
+app.get('/', (req, res) => {
+    const posts = [
+        {
+            id: 1,
+            title: 'Sample Post',
+            content: 'This is a sample post.',
+            userName: 'Alice',
+            comments: [{ userName: 'Bob', comment: 'Great post!' }],
+        },
+    ]; 
+    res.send(posts);
+});
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 
@@ -15,8 +28,11 @@ app.use('/posts', postRoutes);
 app.use('/comments', commentRoutes);
 
 
+app.use((err, req, res, next) => {
+  console.error(err.stack); // Log the error for debugging purposes
+  res.status(500).send('Something went wrong! Please try again later.');
+});
 
-
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`server is running on ${port}`)
 })
